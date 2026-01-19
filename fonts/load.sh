ad8283db67126f8096c072014400ca0d42aa6eea
Andrew McOlash <amcolash@gmail.com>
2026-01-18 23:01:07 -0800

    make fonts a load-able package

---
+++ fonts/load.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# get the script directory
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+DIRNAME=$(basename "$SCRIPT_DIR")
+
+# only install fonts for linux
+if [ "$(uname)" != "Darwin" ]; then
+  echo "[-] Non-linux system detected. Skipping loading."
+  exit 0
+fi
+
+# check if user wants to load brew
+read -p "Would you like to load $DIRNAME? [y/N] " do_load < /dev/tty
+if [[ ! "$do_load" =~ ^[Yy]$ ]]; then
+  echo "[-] Skipping $DIRNAME."
+  exit 0
+fi
+
+# go to the script directory
+pushd "$SCRIPT_DIR" > /dev/null
+
+echo "[+] Installing fonts..."
+
+mkdir -p ~/.local/share/fonts
+
+function install_font() {
+  echo Downloading font $1
+
+  curl -sSL -o font.zip "$1"
+  unzip font.zip -d ~/.local/share/fonts
+  rm -f font.zip
+}
+
+# Grab nerd fonts and install locally
+install_font https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/UbuntuMono.zip
+install_font https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/SourceCodePro.zip
+
+# Refresh font cache
+fc-cache -fv ~/.local/share/fonts
