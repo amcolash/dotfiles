e985b0e72a7caa020e8f91111d0276a3dc5598bb
Andrew McOlash <amcolash@gmail.com>
2026-07-31 19:35:59 -0700

    saved files with arch base finally working

---
+++ flatpak/flatpaks_arch.sh
@@ -0,0 +1,7 @@
+flatpak install flathub cc.arduino.IDE2 -y
+flatpak install flathub com.rustdesk.RustDesk -y
+flatpak install flathub com.usebruno.Bruno -y
+flatpak install flathub com.valvesoftware.SteamLink -y
+flatpak install flathub com.visualstudio.code -y
+flatpak install flathub io.github.cosmic_utils.camera -y
+flatpak install flathub org.siril.Siril -y
