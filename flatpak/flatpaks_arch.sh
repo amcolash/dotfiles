0b867aa3cba06f80ddfffb10c42eb42835bbc3b8
Andrew McOlash <amcolash@gmail.com>
2026-09-02 10:00:32 -0700

    Updates to saved configs

--- flatpak/flatpaks_arch.sh
+++ flatpak/flatpaks_arch.sh
@@ -2,6 +2,6 @@ flatpak install flathub cc.arduino.IDE2 -y
 flatpak install flathub com.rustdesk.RustDesk -y
 flatpak install flathub com.usebruno.Bruno -y
 flatpak install flathub com.valvesoftware.SteamLink -y
-flatpak install flathub com.visualstudio.code -y
 flatpak install flathub io.github.cosmic_utils.camera -y
 flatpak install flathub org.siril.Siril -y
+flatpak install flathub org.upscayl.Upscayl -y
